23cd38f0519f8877512d496ddb45f33eadf94c8d
Lam <daibanglam@gmail.com>
2023-03-16 12:54:32 +0700

    fix: refactor code for different crawling strategy

--- utils.py
+++ utils.py
@@ -1,223 +1,205 @@
 import requests
 from bs4 import BeautifulSoup
 import urllib.parse
-import json
-import pandas as pd
-from os.path import exists
-from os import makedirs
-import string
-import logging
+from typing import Optional, List
+import imdb
+import re
+import datetime
 
-logging.basicConfig(level=logging.INFO)
+class MovieInfo:
+  rating: float
+  genres: List[str]
 
-MOV_LST_FILE = 'data/movies.csv'
-# IMDB_API_KEY = 'k_e9n79f4d' # daibanglam
-IMDB_API_KEY = 'k_x9atwnz9' # gravitylabs
+  def __init__(self, rating: float, genres: List[str]) -> None:
+    self.rating = rating
+    self.genres = genres
 
-def loadMovieList():
-  return pd.read_csv(MOV_LST_FILE, index_col='Netflix Id')
-
-
-def saveMovieList(df):
-  return df.to_csv(MOV_LST_FILE)
+  def __str__(self):
+    return f"rating: {self.rating}, genres: {','.join(self.genres)}"
 
 
-def updateMovieDb():
-  movRecords = getNetflixMovList()
-  if exists(MOV_LST_FILE):
-    df = loadMovieList()
-    newMovies = [mov for mov in movRecords if mov['Netflix Id'] not in df.index]
-    if newMovies:
-      df = pd.concat([df, pd.DataFrame.from_records(newMovies, index='Netflix Id')])
-      logging.info(f"Adding {len(newMovies)} movies")
-  else:
-    df = pd.DataFrame.from_records(movRecords, index='Netflix Id')
+class ApiKeyImdbStrategy:
+  api_key: str
 
-  saveMovieList(df)
-  return df
+  def __init__(self, api_key: str):
+    self.api_key = api_key
 
+  def find_movie(self, title: str, year: Optional[int]) -> Optional[str]:
+    q = urllib.parse.quote(title) + '%20' +str(year)
+    url = f'https://v3.sg.media-imdb.com/suggestion/x/{q}.json?includeVideos=0'
+    response = requests.get(url)
 
-def updateImdbInfo():
-  pass
+    if response.status_code != 200:
+      raise Exception(f'Can not call API, status code: {response.status_code}, error: {response.text}')
 
-def updateMovieRating():
-  df = loadMovieList()
-  # df2 = df[df['Imdb Rating'].isnull() & (df['Movie Type']=='Movie') & (df['Netflix Original']==False)]
-  df2 = df[df['Imdb Rating'].isnull()]
-  for index, mov in df2.iterrows():
-    try:
-      getImdbRating(mov)
-      df.loc[index] = mov
-    except:
-      logging.warning(f"Error when getting movie rating of {mov['Title']}")
-  saveMovieList(df)
+    resp_data = response.json()
+    if len(resp_data['d']) == 0:
+      return None
 
+    movie = resp_data['d'][0]
+    return movie['id'] if movie['y'] == year else None
 
-def updateMovieGenres():
-  df = loadMovieList()
-  df2 = df[df['Genres'].isnull() & df['Imdb Id'].notnull()]
-  for index, mov in df2.iterrows():
-    try:
-      getMovieGenres(mov)
-      df.loc[index] = mov
-    except:
-      logging.warning(f"Error when getting genres of {mov['Title']}")
-  saveMovieList(df)
+  def get_movie_info(self, title: str, year: Optional[int]) -> Optional[MovieInfo]:
+    # https://imdb-api.com/api/
+    # https://imdb-api.com/en/API/UserRatings/k_e9n79f4d/tt4574334
+    mov_id = self.find_movie(title, year)
+    url = f'https://imdb-api.com/en/API/UserRatings/{self.api_key}/{mov_id}'
+    response = requests.request("GET", url)
+    data = response.json()
+    if data['errorMessage']:
+      raise Exception(f"Get imdb rating error: {data['errorMessage']}")
+
+    return  MovieInfo(data['totalRating'], [])
 
 
-def resetMovieData(ids):
-  df = loadMovieList()
-  for index in ids:
-    mov = df.loc[index]
-    mov['Imdb Id'] = ''
-    mov['Imdb Rating'] = ''
-    df.loc[index] = mov
-  saveMovieList(df)
+class HttpImdbStrategy:
+
+  def find_movie(self, title: str, year: Optional[int]) -> Optional[str]:
+    q = urllib.parse.quote(title) + '%20' +str(year)
+    url = f'https://v3.sg.media-imdb.com/suggestion/x/{q}.json?includeVideos=0'
+    response = requests.get(url)
+
+    if response.status_code != 200:
+      raise Exception(f'Can not call API, status code: {response.status_code}, error: {response.text}')
+
+    resp_data = response.json()
+    if len(resp_data['d']) == 0:
+      return None
+    movie = resp_data['d'][0]
+    return movie['id'] if movie['y'] == year else None
+
+  def get_movie_info(self, title: str, year: Optional[int]) -> Optional[MovieInfo]:
+    mov_id = self.find_movie(title, year)
+
+    # https://www.imdb.com/title/tt1959490/
+    url = f'https://www.imdb.com/title/{mov_id}'
+    headers = {
+      'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+    }
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+      raise Exception(f'Can not crawl genres data: {response.status_code}, error: {response.text}')
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    genres = [genreTag.get_text().strip() for genreTag in soup.find_all(class_='iPPPLI')]
+    ratingTag = soup.find(class_='gvYTvP')
+    rating = None if ratingTag is None else float(ratingTag.get_text().strip())
+    return  MovieInfo(rating, genres)
+
+class CinemagoerImdbStrategy:
+  ia = None
+
+  def __init__(self):
+    self.ia = imdb.Cinemagoer()
+
+  def find_movie(self, title: str, year: Optional[int]) -> Optional[str]:
+    movies = self.ia.search_movie(f'{title} {year}')
+    return movies[0].movieID if len(movies) > 0 else None
+
+  def get_movie_info(self, title: str, year: Optional[int]) -> Optional[MovieInfo]:
+    mov_id = self.find_movie(title, year)
+    movie = self.ia.get_movie(mov_id)
+    rating = None if 'rating' not in movie else movie['rating']
+    genres = None if 'genre' not in movie else movie['genre']
+    return  MovieInfo(rating=rating, genres=genres)
 
 
-def getNetflixMovList():
-  cacheKey = 'nflx-mov-list'
-  cacheVal = getCache(cacheKey)
-  if cacheVal:
-    data = cacheVal
-  else:
-    url = 'https://www.netflix.com/nq/website/memberapi/v4c8c6e50/pathEvaluator?avif=false&webp=true&drmSystem=widevine&isVolatileBillboardsEnabled=true&routeAPIRequestsThroughFTL=false&isTop10Supported=true&isTop10KidsSupported=true&hasVideoMerchInBob=true&hasVideoMerchInJaw=true&persoInfoDensity=false&infoDensityToggle=false&contextAwareImages=true&enableMultiLanguageCatalog=false&usePreviewModal=true&falcor_server=0.1.0&withSize=true&materialize=true&original_path=%2Fshakti%2Fv4c8c6e50%2FpathEvaluator'
+class HttpNetflixStrategy:
+  cookie = None
+
+  def __init__(self, cookie: str):
+      self.cookie = cookie
+
+  def get_my_list(self):
+    url = 'https://www.netflix.com/nq/website/memberapi/vc6b505ac/pathEvaluator?avif=false&webp=true&drmSystem=widevine&isVolatileBillboardsEnabled=true&routeAPIRequestsThroughFTL=false&isTop10Supported=true&isTop10KidsSupported=true&hasVideoMerchInBob=true&hasVideoMerchInJaw=true&persoInfoDensity=false&infoDensityToggle=false&contextAwareImages=true&enableMultiLanguageCatalog=false&usePreviewModal=true&falcor_server=0.1.0&withSize=true&materialize=true&original_path=%2Fshakti%2Fmre%2FpathEvaluator'
     maxItemIndex = 1000
     payload=f'path=%5B%22mylist%22%2C%5B%22id%22%2C%22listId%22%2C%22name%22%2C%22requestId%22%2C%22trackIds%22%5D%5D&path=%5B%22mylist%22%2C%7B%22from%22%3A0%2C%22to%22%3A{maxItemIndex}%7D%2C%5B%22availability%22%2C%22episodeCount%22%2C%22inRemindMeList%22%2C%22itemSummary%22%2C%22queue%22%2C%22summary%22%5D%5D&authURL=1656735386949.ilLXtrHw22MnWNrX3jwMASMQZwQ%3D'
     headers = {
       'content-type': 'application/x-www-form-urlencoded',
       'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
-      'Cookie': 'memclid=b783e450-fa77-488c-b3e2-beff624d26da; nfvdid=BQFmAAEBEEVEA6OC5bEMUUnOPrODS8pgtswkpg-fPl9nVBvzgBL6h4xcvm1AW25uw7kCk3ontTzlyk_IFIznVD7tQB97u8Sb9yS3ZTnKWCiKr7ML5MnwT0FDrZ5vU5D__d-e0TaPHF35IjWRoAUBT5CD29YZRJBC; cL=1656564126253%7C165656412298379229%7C165656412237010303%7C%7C4%7Cnull; SecureNetflixId=v%3D2%26mac%3DAQEAEQABABQ4A9El3mFYUqUKCkYGIkttFT-NGkHCdlA.%26dt%3D1656735386889; NetflixId=ct%3DBQAOAAEBEGywe9cwOTu6wOui3wdHeoyCELD3JDrWo1Ixd16GNT19-9WYxRXzn4yAqAWBe7Q8Ts6_z7ZTl93e3RYToHks-d6To43HAqQQGFeRVJrxyWtK1tGio7p5sb43hJJhw3pabc3JVABBdRFdmYuc1T69zRNl7zbaL-MoLO30ztsA8qo9G8eoYprxEYIorpnN1a4ktC_EswHyitH9oDebgmWU0OhtLUleC2sGQg0uAiOFJtrJpiljaigLopsxLjnPj2Fa9DJR4vsIDTN3hzuexkg7q_I1ta9Fc08VDC4JTOA1cRLld1fsl-sGwsCpyjxCu_2dVvRnx-L60KiAD04BdRqfuvLdDu_JACScRcZ-sAHmIAaUiX-_Tmps2mumHjGHCzl42dUjkqXL9N0q4MHC9o4xVLGZ6UMHWv8s12KihVXoaK3WXab7jPnfZ94iTLsUDbpT8Z0LwGxhLv6ckXE9gelc_Pg6GRyOZhnmrTCF_nBD8D4cA3vHhjpJ-4Mwc0eVH_sXn2XpgOSk_zel3lw5KvihnU_m32Tq5jE4Ls7FCAWDtxL9pKXOiVypICrKrIMdNYAh78kYsPgMTWJxua59n0I_FyKCfPPmM1xFcmBSj6kZKFowSYzC3nI0hM4fDB0JT13A7qhWt7DZLmW3DyPlRPGUaEZyBb_YqfV7gLN7pGI-9lVmegmKKpeEv7dJdbMFS6gE9DX58LvE9Zlzj7lIwZhQOkzv4g..%26bt%3Ddbl%26ch%3DAQEAEAABABRZ15hrwaNojow8x1xH56QfC7r5vgbKxCI.%26v%3D2%26mac%3DAQEAEAABABSlloS_xpeNFWOABqutaMm78nLruDvbw1w.; OptanonConsent=isIABGlobal=false&datestamp=Sat+Jul+02+2022+11%3A16%3A28+GMT%2B0700+(Indochina+Time)&version=6.6.0&consentId=22b81ae4-c299-4a0c-ac36-055cb8f2e01c&interactionCount=1&landingPath=NotLandingPage&groups=C0001%3A1%2CC0002%3A1%2CC0003%3A1&hosts=H12%3A1%2CH13%3A1%2CH51%3A1%2CH45%3A1%2CH46%3A1%2CH52%3A1%2CH48%3A1%2CH49%3A1&AwaitingReconsent=false; profilesNewSession=0'
+      'Cookie': self.cookie
     }
     response = requests.request("POST", url, headers=headers, data=payload)
 
     if response.status_code != 200:
       raise Exception(f'Can not call API, status code: {response.status_code}, error: {response.text}')
-    data = response.json()
-    setCache(cacheKey, data)
+    myListData = response.json()
+    items = []
+    for movId in myListData['jsonGraph']['videos']:
+      # itemSummary / value {
+      #     episodeCount, seasonCount, isOriginal, releaseYear
+      #     boxArt / { url, width, height }
+      #     maturity / rating / { value, specificRatingReason, maturityLevel }
+      #   }
+      video = myListData['jsonGraph']['videos'][movId]
+      episode_cnt = video['itemSummary']['value']['seasonCount'] if 'seasonCount' in video['itemSummary']['value'] else None
+      item = {
+        'Netflix Id': video['itemSummary']['value']['id'],
+        'Title': video['itemSummary']['value']['title'],
+        'Release Year': video['itemSummary']['value']['releaseYear'],
+        'Netflix Original': video['itemSummary']['value']['isOriginal'],
+        'Episode Count': video['itemSummary']['value']['episodeCount'] if 'episodeCount' in video['itemSummary']['value'] else None,
+        'Season Count': episode_cnt,
+        'Maturity': video['itemSummary']['value']['maturity']['rating']['value'],
+        'Maturity Level': video['itemSummary']['value']['maturity']['rating']['maturityLevel'],
+        'Maturity Detail': video['itemSummary']['value']['maturity']['rating']['specificRatingReason'],
+        'Image': video['itemSummary']['value']['boxArt']['url'],
+        'Movie Type': 'Movie' if episode_cnt is None else 'TV Series'
+      }
+      items.append(item)
+    return items
 
-  result = convertNetflixMoviesToRecords(data)
-  return result
+  def get_last_date_to_watch(self, movie_id):
+    print(f'getting last date to watch for movie {movie_id}')
+    url = f'https://www.netflix.com/title/{movie_id}'
+    # url = f'https://www.netflix.com/title/60020686'
+    headers = {
+      'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+      'Cookie': self.cookie
+    }
+    response = requests.get(url, headers=headers)
 
+    if response.status_code != 200:
+      raise Exception(f'Can not call API, status code: {response.status_code}, error: {response.text}')
 
-def getImdbRating(mov):
-  # https://imdb-api.com/api/
-  # https://imdb-api.com/en/API/UserRatings/k_e9n79f4d/tt4574334
-  title = mov['Title']
-  getImdbMovId(mov)
-  imdbId = mov['Imdb Id']
+    pattern = r'Last\\x20day\\x20to\\x20watch\\x20on\\x20Netflix:\\x20([A-Za-z]+)\\x20(\d+)'
+    match = re.search(pattern, response.text)
+    expire_date = None
+    if match:
+        month_name = match.group(1)
+        day = int(match.group(2))
+        month = datetime.datetime.strptime(month_name, "%B").month
+        current_date_time = datetime.date.today()
+        expire_date = current_date_time.replace(month=month, day=day)
+    return expire_date
 
-  cacheKey = f'imdb-rating-{title}'
-  cacheVal = getCache(cacheKey)
-  if cacheVal:
-    data = cacheVal
-  else:
-    url = f'https://imdb-api.com/en/API/UserRatings/{IMDB_API_KEY}/{imdbId}'
-    response = requests.request("GET", url)
-    data = response.json()
-    if data['errorMessage']:
-      raise Exception(f"Get imdb rating error: {data['errorMessage']}")
-    setCache(cacheKey, data)
+# def getImdbMovId(mov):
+#   # https://imdb-api.com/api/
+#   title = mov['Title']
 
-  mov['Imdb Rating'] = data['totalRating']
+#   cacheKey = f'imdb-id-{title}'
+#   cacheVal = getCache(cacheKey)
+#   if cacheVal:
+#     data = cacheVal
+#   else:
+#     search = urllib.parse.quote(f"{title} {mov['Release Year']}")
+#     # search = urllib.parse.quote(title)
+#     if mov['Movie Type'] == 'Movie':
+#       url = f'https://imdb-api.com/en/API/SearchMovie/{IMDB_API_KEY}/{search}'
+#     else:
+#       url = f'https://imdb-api.com/en/API/SearchSeries/{IMDB_API_KEY}/{search}'
+#     logging.info(f'Finding movie with title: "{title}"')
+#     response = requests.request("GET", url)
+#     data = response.json()
 
+#     # TODO: check release year in movie description
 
-def getImdbMovId(mov):
-  # https://imdb-api.com/api/
-  title = mov['Title']
+#     if data['errorMessage']:
+#       raise Exception(f"Get imdb movie id error: {data['errorMessage']}")
+#     setCache(cacheKey, data)
 
-  cacheKey = f'imdb-id-{title}'
-  cacheVal = getCache(cacheKey)
-  if cacheVal:
-    data = cacheVal
-  else:
-    search = urllib.parse.quote(f"{title} {mov['Release Year']}")
-    # search = urllib.parse.quote(title)
-    if mov['Movie Type'] == 'Movie':
-      url = f'https://imdb-api.com/en/API/SearchMovie/{IMDB_API_KEY}/{search}'
-    else:
-      url = f'https://imdb-api.com/en/API/SearchSeries/{IMDB_API_KEY}/{search}'
-    logging.info(f'Finding movie with title: "{title}"')
-    response = requests.request("GET", url)
-    data = response.json()
+#   if len(data['results']) < 1:
+#     raise Exception(f'Movie with title {title} not found')
 
-    # TODO: check release year in movie description
-
-    if data['errorMessage']:
-      raise Exception(f"Get imdb movie id error: {data['errorMessage']}")
-    setCache(cacheKey, data)
-
-  if len(data['results']) < 1:
-    raise Exception(f'Movie with title {title} not found')
-
-  mov['Imdb Id'] = data['results'][0]['id']
-
-
-def getMovieGenres(mov):
-  imdbMovId = mov['Imdb Id']
-  logging.info(f"Getting genre of {mov['Title']} movies")
-  # https://www.imdb.com/title/tt1959490/
-  url = f'https://www.imdb.com/title/{imdbMovId}'
-  response = requests.request("GET", url)
-  soup = BeautifulSoup(response.text, 'html.parser')
-  genres = [genreTag.get_text().strip() for genreTag in soup.find_all(class_='ipc-chip__text')]
-  mov['Genres'] = ','.join(genres)
-
-
-def convertNetflixMoviesToRecords(data):
-  # jsonGraph / videos / {80229873}
-  videos = data['jsonGraph']['videos']
-  movies = [ convertMovieItemToRecord(videos[mvId]) for mvId in videos]
-  return movies
-
-
-def convertMovieItemToRecord(video):
-  # itemSummary / value {
-  #     episodeCount, seasonCount, isOriginal, releaseYear
-  #     boxArt / { url, width, height }
-  #     maturity / rating / { value, specificRatingReason, maturityLevel }
-  #   }
-  epCnt = video['itemSummary']['value']['seasonCount'] if 'seasonCount' in video['itemSummary']['value'] else None
-  return {
-    'Netflix Id': video['itemSummary']['value']['id'],
-    'Title': video['itemSummary']['value']['title'],
-    'Release Year': video['itemSummary']['value']['releaseYear'],
-    'Netflix Original': video['itemSummary']['value']['isOriginal'],
-    'Episode Count': video['itemSummary']['value']['episodeCount'] if 'episodeCount' in video['itemSummary']['value'] else None,
-    'Season Count': epCnt,
-    'Maturity': video['itemSummary']['value']['maturity']['rating']['value'],
-    'Maturity Level': video['itemSummary']['value']['maturity']['rating']['maturityLevel'],
-    'Maturity Detail': video['itemSummary']['value']['maturity']['rating']['specificRatingReason'],
-    'Image': video['itemSummary']['value']['boxArt']['url'],
-    'Movie Type': 'Movie' if epCnt is None else 'TV Series'
-  }
-
-
-CACHE_DIR = 'cache'
-
-
-def sanitizeFileName(s):
-  valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
-  return ''.join(c for c in s.replace(" ", "-").lower() if c in valid_chars)
-
-def getCache(key):
-  cacheFile = f'{CACHE_DIR}/{sanitizeFileName(key)}.json'
-
-  if not exists(cacheFile):
-    return None
-
-  f = open(cacheFile)
-  data = json.load(f)
-  f.close()
-  return data
-
-
-def setCache(key, value):
-  cacheFile = f'{CACHE_DIR}/{sanitizeFileName(key)}.json'
-
-  if not exists(CACHE_DIR):
-    makedirs(CACHE_DIR)
-
-  out_file = open(cacheFile, "w", encoding='utf-8')
-  json.dump(value, out_file, indent = 2)
-  out_file.close()
+#   mov['Imdb Id'] = data['results'][0]['id']
